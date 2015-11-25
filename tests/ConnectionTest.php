740370bf16ded0971de6145ad01a6701a0b1015a
Fabian Vogler <fabian@equivalence.ch>
2015-11-25 14:59:58 +0100

    Add PHPUnit tests

---
+++ tests/ConnectionTest.php
@@ -0,0 +1,23 @@
+<?php
+
+use Silex\WebTestCase;
+
+class ConntextionTest extends WebTestCase
+{
+    public function createApplication()
+    {
+        $app = require __DIR__.'/../src/app.php';
+        $app['debug'] = true;
+        unset($app['exception_handler']);
+        return $app;
+    }
+
+    public function testHome()
+    {
+        $client = $this->createClient();
+        $crawler = $client->request('GET', '/');
+
+        $this->assertTrue($client->getResponse()->isOk());
+        $this->assertContains('Swiss Public Transport', $crawler->filter('h1')->text());
+    }
+}
