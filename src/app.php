11f17c2ea32760e43c003ddbff0f65910d67cec8
Fabian Vogler <fabian@equivalence.ch>
2020-05-24 11:10:15 +0000

    Apply fixes from StyleCI

--- src/app.php
+++ src/app.php
@@ -6,7 +6,6 @@ use Silex\Application;
 use Silex\Provider\TwigServiceProvider;
 use Symfony\Component\Debug\ErrorHandler;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\HttpKernelInterface;
 
 ErrorHandler::register();
